4ca48cfe5de3708dd3f2cf898bbbd13145ba248a
diyradiolab <diyradiolab@gmail.com>
2025-05-10 23:51:04 -0400

    test
    test

--- patio-cover-app/vite.config.js
+++ patio-cover-app/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  base: '/PatioRender/', // Replace with your repository name
-  build: {
-    outDir: 'dist'
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+})
